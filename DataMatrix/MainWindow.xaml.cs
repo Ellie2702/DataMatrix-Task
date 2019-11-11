ce27e38cf05182122b6115bfb8d33624f0ef0fd1
Ellie2702 <allikatyapaeva@gmail.com>
2019-11-12 01:01:50 +0300

    don't laught :c

--- DataMatrix/MainWindow.xaml.cs
+++ DataMatrix/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace DataMatrix
 {
@@ -39,95 +42,191 @@ namespace DataMatrix
             {
                 for (int j = 0; j <= arr.GetUpperBound(1); j++)
                 {
-                    if (i == 0 )
-                    {
-                        arr[i,j] = 1;
-                    } else if (j == arr.GetUpperBound(1))
+                    if (i == 0)
                     {
                         arr[i, j] = 1;
-                    } else if (j == 0 && i % 2 == 0)
+                    }
+                    else if (j == arr.GetUpperBound(1))
                     {
                         arr[i, j] = 1;
-                    } else if (i == arr.GetUpperBound(0) && j % 2 == 0)
+                    }
+                    else if (j == 0 && i % 2 == 0)
                     {
                         arr[i, j] = 1;
-                    } else arr[i, j] = 0;
+                    }
+                    else if (i == arr.GetUpperBound(0) && j % 2 == 0)
+                    {
+                        arr[i, j] = 1;
+                    }
+                    else arr[i, j] = 0;
                 }
             }
 
             int bit = 8;
             int CurY = 0, curX = 0;
+            string dir = "up";
+
             for (int i = 0; i < bin.Length; i++)
             {
-                    
                 switch (bit)
                 {
                     case 8:
-                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
                         curX = x;
                         CurY = y;
                         y--;
-                        if(y <= 0){
-                            x += 2;
+                        bit--;
+                        if (y <= 0)
+                        {
+                            x -= 2;
                             y = arr.GetUpperBound(0) - 1;
                         }
                         break;
 
                     case 7:
-                        arr[y, x] = Convert.ToInt32(bin[i]);
-                        x--;
-                        if(y <= 0){
-                            x += 2;
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
+                        y--;
+                        bit--;
+                        if (y <= 0)
+                        {
+                            x -= 2;
                             y = arr.GetUpperBound(0) - 1;
                         }
                         break;
 
                     case 6:
-                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
                         y = CurY;
                         x++;
-                        if(x <= 0){
+                        bit--;
+                        if (x <= 0)
+                        {
                             x = arr.GetUpperBound(1) - 1;
                             y -= 2;
-                            if(y <= 0){
+                            if (y <= 0)
+                            {
                                 y = arr.GetUpperBound(0) - 1;
                             }
                         }
                         break;
 
                     case 5:
-                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
                         y--;
-                        if(y <= 0){
-                            x += 2;
+                        bit--;
+                        if (y <= 0)
+                        {
+                            x -= 2;
                             y = arr.GetUpperBound(0) - 1;
                         }
                         break;
 
                     case 4:
-                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
                         y--;
-                        if(y <= 0){
-                            x += 2;
+                        bit--;
+                        if (y <= 0)
+                        {
+                            x -= 2;
                             y = arr.GetUpperBound(0) - 1;
-                        }            
+                        }
                         break;
 
                     case 3:
-
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
+                        y = CurY - 1;
+                        x++;
+                        bit--;
+                        if (x <= 0)
+                        {
+                            x = arr.GetUpperBound(1) - 1;
+                            y -= 2;
+                            if (y <= 0)
+                            {
+                                y = arr.GetUpperBound(0) - 1;
+                            }
+                        }
                         break;
 
                     case 2:
-
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
+                        y--;
+                        bit--;
+                        if (y <= 0)
+                        {
+                            x -= 2;
+                            y = arr.GetUpperBound(0) - 1;
+                        }
                         break;
 
                     case 1:
-
+                        arr[y, x] = Convert.ToInt32(bin[i].ToString());
+                        
+                        if (dir == "up")
+                        {
+                            if (curX - 2 <= 0)
+                            {
+                                x = curX + 2;
+                                y = CurY - 2;
+                                dir = "down";
+                            } else if(CurY - 2 >= arr.GetUpperBound(0)) {
+                                x = curX + 2;
+                                y = CurY - 2;
+                                dir = "down";
+                            } 
+                            else if (curX - 2 <= 0 && CurY + 2 >= arr.GetUpperBound(0))
+                            {
+                                x = curX + 2;
+                                y = CurY - 2;
+                                dir = "down";
+                            }
+                            else if (curX == 0 - 1 && CurY + 2 <= arr.GetUpperBound(0) - 1)
+                            {
+                                y = CurY + 3;
+                                x = curX;
+                                dir = "down";
+                            }
+                            else
+                            {
+                                x = curX - 2;
+                                y = CurY + 2;
+                            }
+                        }
+                        else if (dir == "down")
+                        {
+                            if (curX - 2 >= arr.GetUpperBound(1) && CurY - 2 <= 0)
+                            {
+                                x = curX - 2;
+                                y = CurY + 2;
+                                dir = "up";
+                            }
+                            else if (curX - 2 >= arr.GetUpperBound(1))
+                            {
+                                x = curX - 2;
+                                y = CurY + 2;
+                                dir = "up";
+                            }
+                            else if (CurY - 2 <= 0)
+                            {
+                                x = curX - 2;
+                                y = CurY + 2;
+                                dir = "up";
+                            }
+                            else
+                            {
+                                x = curX + 2;
+                                y = CurY - 2;
+                            }
+                        }
+                        bit = 8;
                         break;
+                        
+                }
 
             }
-
         }
+
+        
 
 
         private void Go_Click(object sender, RoutedEventArgs e)
@@ -175,7 +274,7 @@ namespace DataMatrix
 
 
 
-                int[] c = new int[Text.Length + r];
+                int[] c = new int[f];
                 int j = 0;
                 string k = "";
                 int p = 0;
@@ -206,7 +305,7 @@ namespace DataMatrix
                 }
 
                 //add margin
-                if (r > 0)
+                if (f - c.Length != 0)
                 {
                     c[j] = 129;
                     for (int i = j + 1; i < c.Length; i++)
@@ -245,11 +344,56 @@ namespace DataMatrix
                     }
                 }
 
-                MessageBox.Show(res);
+                //byte[] bytes = new byte[f * f];
+                //int b = 0;
+                //for(int i = 0; i < f; i++){
+                //    for (int y = 0; y < f; y++)
+                //    {
+                //        bytes[b] = Convert.ToByte(arr[i, y].ToString() + arr[i, j + 1].ToString() + arr[i, j + 2].ToString() + arr[i, y + 3].ToString() + arr[i, j + 4].ToString() + arr[i, j + 5].ToString() + arr[i, j + 6].ToString() + arr[i, j + 7].ToString());
+                //            b++;
+                //            y += 6;
+                //    }
+                //}
+
+
+                var box = new PictureBox();
+                hospade.Child = box;
+                Bitmap img = new Bitmap(128, 128);
+                Graphics g = Graphics.FromImage(img);
+                System.Drawing.Brush ff = System.Drawing.Brushes.Black;
+
+
+                int count = 128 / f;
+                for (int i = 0; i < count; i++)
+                {
+                    for (int sa = 0; sa < count; sa++)
+                    {
+                        if (arr[i, sa] == 1)
+                        {
+                            g.FillRectangle(ff, i * count, sa * count, count, count);
+                        }
+                    }
+
+                }
+
+                g.Flush();
+
+                box.Image = img;
+
+                //var stream = new MemoryStream(bytes);
+                //var image = new BitmapImage();
+                //image.BeginInit();
+                //image.StreamSource = stream;
+                //image.EndInit();
+
+
+                //DM.Source = image;
+
+               // MessageBox.Show(res);
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                System.Windows.Forms.MessageBox.Show(ex.Message);
             }
            
         }
