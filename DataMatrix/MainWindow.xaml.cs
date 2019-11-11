099cccdc9713e3785ea2a620e540f69ffa1895a5
Ellie2702 <allikatyapaeva@gmail.com>
2019-11-11 22:34:56 +0300

    MapMatrix p1

--- DataMatrix/MainWindow.xaml.cs
+++ DataMatrix/MainWindow.xaml.cs
@@ -31,6 +31,104 @@ namespace DataMatrix
 
         }
 
+        public void MapMatrix(string bin, int[,] arr)
+        {
+            int x = 1, y = 5;
+
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    if (i == 0 )
+                    {
+                        arr[i,j] = 1;
+                    } else if (j == arr.GetUpperBound(1))
+                    {
+                        arr[i, j] = 1;
+                    } else if (j == 0 && i % 2 == 0)
+                    {
+                        arr[i, j] = 1;
+                    } else if (i == arr.GetUpperBound(0) && j % 2 == 0)
+                    {
+                        arr[i, j] = 1;
+                    } else arr[i, j] = 0;
+                }
+            }
+
+            int bit = 8;
+            int CurY = 0, curX = 0;
+            for (int i = 0; i < bin.Length; i++)
+            {
+                    
+                switch (bit)
+                {
+                    case 8:
+                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        curX = x;
+                        CurY = y;
+                        y--;
+                        if(y <= 0){
+                            x += 2;
+                            y = arr.GetUpperBound(0) - 1;
+                        }
+                        break;
+
+                    case 7:
+                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        x--;
+                        if(y <= 0){
+                            x += 2;
+                            y = arr.GetUpperBound(0) - 1;
+                        }
+                        break;
+
+                    case 6:
+                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        y = CurY;
+                        x++;
+                        if(x <= 0){
+                            x = arr.GetUpperBound(1) - 1;
+                            y -= 2;
+                            if(y <= 0){
+                                y = arr.GetUpperBound(0) - 1;
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        y--;
+                        if(y <= 0){
+                            x += 2;
+                            y = arr.GetUpperBound(0) - 1;
+                        }
+                        break;
+
+                    case 4:
+                        arr[y, x] = Convert.ToInt32(bin[i]);
+                        y--;
+                        if(y <= 0){
+                            x += 2;
+                            y = arr.GetUpperBound(0) - 1;
+                        }            
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 1:
+
+                        break;
+
+            }
+
+        }
+
 
         private void Go_Click(object sender, RoutedEventArgs e)
         {
@@ -39,7 +137,7 @@ namespace DataMatrix
                 string Text = Data.Text;
                 // 
                 int r = 0;
-                //
+                // size
                 int f = 0;
 
 
@@ -48,30 +146,30 @@ namespace DataMatrix
                 {
 
                     r = 3 - Text.Length;
-                    f = 3;
+                    f = 10;
                 }
                 else if (Text.Length <= 5)
                 {
 
                     r = 5 - Text.Length;
-                    f = 5;
+                    f = 12;
                 }
                 else if (Text.Length <= 8)
                 {
 
                     r = 8 - Text.Length;
-                    f = 8;
+                    f = 14;
                 }
                 else if (Text.Length <= 12)
                 {
 
                     r = 12 - Text.Length;
-                    f = 12;
+                    f = 16;
                 }
                 else if (Text.Length <= 18)
                 {
 
-                    r = 18 - Text.Length;
+                    r = 16 - Text.Length;
                     f = 18;
                 }
 
@@ -127,10 +225,27 @@ namespace DataMatrix
                 for (int i = 0; i < c.Length; i++)
                 {
                     binary += Convert.ToString(c[i], 2);
-                    
                 }
 
-                DM.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(binary);
+                int[,] arr = new int[f,f];
+
+                string res = "";
+
+                MapMatrix(binary, arr);
+
+                for (int i = 0; i < f; i++)
+                {
+                    for (int z = 0; z < f; z++)
+                    {
+                        if (z != f - 1)
+                        {
+                            res += arr[z, i] + " ";
+                        }
+                        else res += arr[z, i] + Environment.NewLine; 
+                    }
+                }
+
+                MessageBox.Show(res);
             }
             catch (Exception ex)
             {
